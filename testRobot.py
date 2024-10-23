7eabf9afec4fd7392ea290d2ff28dc2c92ea6522
Frikk Brændsrød <frikkb@ntnu.no>
2024-10-23 19:45:24 +0200

    Copied test function over to external file.

---
+++ testRobot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+import time as t
+
+# Creating objects
+ev3 = EV3Brick()
+
+
+class wasteDisposalRobot:
+    def __init__(self):
+        # Initializing motors.
+        self.leftBelt = Motor(Port.C, Direction.CLOCKWISE)
+        self.rightBelt = Motor(Port.B, Direction.CLOCKWISE)
+
+        self.claw = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+
+        # self.colorSensor = ColorSensor(Port.S1)
+        self.distanceSensor = UltrasonicSensor(Port.S2)
+
+        # Initializing the drivebase.
+        self.robot = DriveBase(self.leftBelt, self.rightBelt, wheel_diameter=56,
+                               axle_track=114)  # Add wheel_diameter and axle_track for accurate movement
+
+    def drive(self, speed_left, speed_right):
+        # Drive the robot with a specific speed for the left and right wheels
+        self.robot.drive(speed_left, speed_right)
+
+    def grip(self):
+        # self.claw.run_angle(90,90)
+        self.claw.run(400)
+        # self.claw.run_target(50, 0)  # Assuming 0 is the closed position
+
+    def release(self):
+        self.claw.run_target(50, 90)  # Assuming 90 is the open position
+
+
+def testRobot():
+    robot = wasteDisposalRobot()
+
+    while robot.distanceSensor.distance() > 200:
+        #(Straight, turn(+ = R,   - = L))
+        robot.drive(200, 0)
+        robot.grip()
+        #robot.release
+
+    robot.drive(0, 0)
+
+
+# Run the test
+testRobot()
