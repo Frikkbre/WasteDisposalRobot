30bd5a848fe28dbc91e1cd792c7f19d3d6255b38
Frikk Brændsrød <frikkb@ntnu.no>
2024-10-23 12:47:23 +0200

    Added framework for ev3dev

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
-print("Frikk")
-=======
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+import time as t
 
+# Creating objects
+ev3 = EV3Brick()
 
-
-
-
-
-
-
-
-print("Test Erik")
-
-
-
-
-print("Test Kristian")
->>>>>>> b495f5e69c6184ec224fd0cdbf7f762802d0cfed
+# Initializing motors.
+leftMotor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+rightMotor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
